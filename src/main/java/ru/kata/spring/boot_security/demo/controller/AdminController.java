4b2318e164f0a3cd5164c2a456da4f08d567ba14
Anton Smirnov <smiru2000@gmail.com>
2023-05-14 19:02:56 +0300

    Added Edit and Delete using Bootstrap-modal

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,10 +3,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.constant.RoleConst;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
@@ -18,6 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/admin", path = "/admin")
 public class AdminController {
 
     private final UserService userService;
@@ -32,33 +30,33 @@ public class AdminController {
         return RoleConst.values();
     }
 
-    @GetMapping(value = {"/admin", "/admin/users"})
-    public String printWelcome(ModelMap model, @ModelAttribute(name = "newUser") User newUser) {
+    @GetMapping(value = {"", "/users"})
+    public String printWelcome(ModelMap model, @ModelAttribute(name = "newUser") User newUser, @ModelAttribute(name = "editUser") User editUser) {
         model.addAttribute("users", userService.listUsers());
         return "users";
     }
 
-    @PostMapping(value = "admin/users")
+    @PostMapping(value = {"", "/users"})
     public String createUser(ModelMap model, @ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/admin/users";
     }
 
-    @PostMapping(value = "admin/users/delete/{id}")
+    @PostMapping(value = {"/delete/{id}","/users/delete/{id}"})
     public String deleteUser(ModelMap model, @PathVariable("id") long id) {
         userService.deleteUserById(id);
         model.addAttribute("users", userService.listUsers());
         return "redirect:/admin/users";
     }
 
-    @GetMapping(value = "admin/users/edit/{id}")
-    public String getUser(ModelMap model, @PathVariable("id") long id) {
-        model.addAttribute("user", userService.findUserById(id));
-        return "user-edit";
-    }
+//    @GetMapping(value = "admin/users/edit/{id}")
+//    public String getUser(ModelMap model, @PathVariable("id") long id) {
+//        model.addAttribute("user", userService.findUserById(id));
+//        return "user-edit";
+//    }
 
-    @PostMapping(value = "admin/users/edit/{id}")
-    public String editUser(ModelMap model, @ModelAttribute("user") User user) {
+    @PostMapping(value = {"/edit/{id}","/users/edit/{id}"})
+    public String editUser(ModelMap model, @ModelAttribute("editUser") User user) {
         userService.updateUser(user);
         model.addAttribute("users", userService.listUsers());
         return "redirect:/admin/users";
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,52 @@
 $( document ).ready( function () {
-    var url = window.location.href.substr( window.location.href.lastIndexOf( '/' ) );
+
     $( 'a' ).each( function () {
-        console.log(this);
-        if( $( this ).attr( 'href' ) === url &&  $( this ).parent( 'div' ).hasClass('navbar-side')) {
+        if( $( this ).parent( 'div' ).hasClass('navbar-side') && ((document.location.pathname === ($( this ).attr( 'href' ))) || (document.location.pathname.indexOf($( this ).attr( 'href' )+'/') != -1)) ) {
             $( this ).parent( 'div' ).addClass( 'active' );
         }
     });
 });
+
+$(function() {
+    //Take the data from the TR during the event button
+    $('table#usersList').on('click', '#editUserButton',function (ele) {
+        var id = cloneData(ele);
+        $('#userInfoModalLabel').html('Edit user: ');
+        $("#userInfoForm").attr('action', '/admin/users/edit/'+id);
+        $("#userInfoForm fieldset").attr('disabled', false);
+        $('#saveModalButton').removeClass('btn-danger');
+        $('#saveModalButton').addClass('btn-primary');
+        $('#saveModalButton').html('Save');
+    });
+    $('table#usersList').on('click', '#deleteUserButton',function (ele) {
+        var id = cloneData(ele);
+        $('#userInfoModalLabel').html('Delete user: ');
+        $("#userInfoForm").attr('action', '/admin/users/delete/'+id);
+        $("#userInfoForm fieldset").attr('disabled', true);
+        $('#saveModalButton').addClass('btn-danger');
+        $('#saveModalButton').removeClass('btn-primary');
+        $('#saveModalButton').html('Delete');
+    });
+});
+
+function cloneData(table) {
+    var tr = table.target.closest('tr');
+    //I get the value from the cells (td) using the parentNode (var tr)
+    var id = tr.cells[0].textContent;
+    var firstName = tr.cells[1].textContent;
+    var lastName = tr.cells[2].textContent;
+    var email = tr.cells[3].textContent;
+    var userName = tr.cells[4].textContent;
+    var password = tr.cells[5].textContent;
+    var enabled = $(tr.cells[6]).find('input').attr('checked');
+
+    //Prefill the fields with the gathered information
+    $('#editId').val(id);
+    $('#editFirstname').val(firstName);
+    $('#editLastname').val(lastName);
+    $('#editEmail').val(email);
+    $('#editUsername').val(userName);
+    $('#editPassword').val(password);
+    $('#editEnabled').attr('checked',enabled);
+    return id;
+}
